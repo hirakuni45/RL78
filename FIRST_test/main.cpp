b228c0a95888df63c7b7cd17a9d910e2929ac0ed
hira <hira@rvf-rc45.net>
2016-07-10 03:37:55 +0900

    refactoring at io_utils.hpp

--- FIRST_test/main.cpp
+++ FIRST_test/main.cpp
@@ -1,38 +1,48 @@
+//=====================================================================//
+/*!	@file
+	@brief	LED 点滅 @n
+			LED は、P43(2) に接続 @n
+			ex: Vcc = LED(A), P43 = LED(K) 吸い込み @n
+			    ※必ず直列に適等な抵抗を入れて電流を 3mA 程度に調整する。
+	@author	平松邦仁 (hira@rvf-rc45.net)
+*/
+//=====================================================================//
 #include <stdint.h>
 #include "common/io_utils.hpp"
 
-// P43(2) に LED を繋ぐ
-
 namespace device {
 
-	typedef io8<0xfff04> p4_io;
-	struct p4_t : public p4_io {
-		using p4_io::operator =;
-		using p4_io::operator ();
-		using p4_io::operator |=;
-		using p4_io::operator &=;
+	typedef rw8_t<0xfff04> p4_rw;
+	struct p4_t : public p4_rw {
+		using p4_rw::operator =;
+		using p4_rw::operator ();
+		using p4_rw::operator |=;
+		using p4_rw::operator &=;
 
-		bit_t<p4_io, 3> B3;
+		bit_rw_t<p4_rw, 3> B3;
 	};
 	static p4_t P4;
 
 
-	typedef io8<0xfff24> pm4_io;
-	struct pm4_t : public pm4_io {
-		using pm4_io::operator =;
-		using pm4_io::operator ();
-		using pm4_io::operator |=;
-		using pm4_io::operator &=;
+	typedef rw8_t<0xfff24> pm4_rw;
+	struct pm4_t : public pm4_rw {
+		using pm4_rw::operator =;
+		using pm4_rw::operator ();
+		using pm4_rw::operator |=;
+		using pm4_rw::operator &=;
 
-		bit_t<pm4_io, 3> B3;
+		bit_rw_t<pm4_rw, 3> B3;
 	};
 	static pm4_t PM4;
 
 }
 
-static void wait_()
-{
-	asm("nop");
+
+namespace {
+	void wait_()
+	{
+		asm("nop");
+	}
 }
 
 
