af86a71d71ede2d7e7e6e1adb3ab706aa964a231
hira <hira@rvf-rc45.net>
2016-07-20 12:27:13 +0900

    new file

---
+++ G13/timer.hpp
@@ -0,0 +1,30 @@
+#pragma once
+//=====================================================================//
+/*!	@file
+	@brief	RL78/G13 グループ・１２ビット・インターバル・タイマー定義 @n
+			Copyright 2016 Kunihito Hiramatsu
+	@author	平松邦仁 (hira@rvf-rc45.net)
+*/
+//=====================================================================//
+#include "common/io_utils.hpp"
+
+namespace device {
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief  １２ビット・インターバル・タイマ・コントロール・レジスタ（ITMC）
+		@param[in]	T	アクセス・クラス
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	template <class T>
+	struct itmc_t : public T {
+		using T::operator =;
+		using T::operator ();
+		using T::operator |=;
+		using T::operator &=;
+
+		bit_rw_t<T, 15>	    RINTE;  ///< 12ビット・インターバル・タイマの動作制御
+		bits_rw_t<T, 0, 12> ITCMP;  ///< 12ビット・インターバル・タイマのコンペア値設定
+	};
+	static itmc_t< rw16_t<0xFFF90> > ITMC;
+}
