e1b379a9f92ddf312bef8d12154ece9795413661
hira <hira@rvf-rc45.net>
2016-07-24 14:50:53 +0900

    test delay

--- TMPtest/main.cpp
+++ TMPtest/main.cpp
@@ -6,43 +6,21 @@
 //=====================================================================//
 #include <cstdint>
 #include "G13/port.hpp"
+#include "common/delay.hpp"
+#include "common/itimer.hpp"
 
 namespace {
-	void wait_()
-	{
-		asm("nop");
-	}
 
-	void (*task_)();
 }
-
-namespace device {
-	static basic_rw_t<rw8_t<0xFFFA5> > CKS0;
-	static basic_rw_t<rw8_t<0xFFFA6> > CKS1;
-}
-
 
 int main(int argc, char* argv[])
 {
-	task_ = wait_;
-
 	using namespace device;
-
-	CKS0 = 0x87;
-	CKS1 = 0x86;
-
-	PM14.B0 = 0;
-	PM14.B1 = 0;
 
 	PM4.B3 = 0;  // output
 
-	bool f = false;
-	uint32_t n = 0;
 	while(1) {
-		for(uint32_t i = 0; i < 1000000; ++i) {
-			(*task_)();
-		}
-		P4.B3 = f;
-		f = !f;
+		utils::delay::micro_second(10);
+		P4.B3 = !P4.B3();
 	}
 }
