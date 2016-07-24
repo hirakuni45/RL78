a3ab74fb9a74125ff23efe870af2fa484b33eaea
hira <hira@rvf-rc45.net>
2016-07-24 13:35:43 +0900

    new project

---
+++ INTERVAL_TIMER_test/main.cpp
@@ -0,0 +1,65 @@
+//=====================================================================//
+/*!	@file
+	@brief	インターバル・タイマー・テスト
+	@author	平松邦仁 (hira@rvf-rc45.net)
+*/
+//=====================================================================//
+#include <cstdint>
+#include "G13/port.hpp"
+#include "common/delay.hpp"
+#include "common/itimer.hpp"
+
+namespace {
+	device::itimer<uint8_t> itm_;
+}
+
+// 割り込みベクターの設定
+const void* ivec_[] __attribute__ ((section (".ivec"))) = {
+	/*  0 */  nullptr,
+	/*  1 */  nullptr,
+	/*  2 */  nullptr,
+	/*  3 */  nullptr,
+	/*  4 */  nullptr,
+	/*  5 */  nullptr,
+	/*  6 */  nullptr,
+	/*  7 */  nullptr,
+	/*  8 */  nullptr,
+	/*  9 */  nullptr,
+	/* 10 */  nullptr,
+	/* 11 */  nullptr,
+	/* 12 */  nullptr,
+	/* 13 */  nullptr,
+	/* 14 */  nullptr,
+	/* 15 */  nullptr,
+	/* 16 */  nullptr,
+	/* 17 */  nullptr,
+	/* 18 */  nullptr,
+	/* 19 */  nullptr,
+	/* 20 */  nullptr,
+	/* 21 */  nullptr,
+	/* 22 */  nullptr,
+	/* 23 */  nullptr,
+	/* 24 */  nullptr,
+	/* 25 */  nullptr,
+	/* 26 */  reinterpret_cast<void*>(itm_.task),
+};
+
+
+int main(int argc, char* argv[])
+{
+	using namespace device;
+
+	PM4.B3 = 0;  // output
+
+	uint8_t intr_level = 1;
+	itm_.start(60, intr_level);
+
+	uint16_t n = 0;
+	while(1) {
+///		utils::delay::micro_second(10);
+		itm_.sync();
+		++n;
+		if(n >= 30) n = 0;
+		P4.B3 = n < 10 ? false : true; 	
+	}
+}
