436a6cec154b3763dee4ed3954e1d1540691a804
hira <hira@rvf-rc45.net>
2016-07-24 13:36:46 +0900

    new file

---
+++ common/itimer.hpp
@@ -0,0 +1,112 @@
+#pragma once
+//=====================================================================//
+/*!	@file
+	@brief	RL78/G13 グループ 12 ビット・インターバルタイマー制御 @n
+			Copyright 2016 Kunihito Hiramatsu
+	@author	平松邦仁 (hira@rvf-rc45.net)
+*/
+//=====================================================================//
+#include "G13/timer.hpp"
+#include "G13/system.hpp"
+#include "G13/intr.hpp"
+
+namespace device {
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief  １２ビット・インターバル・タイマー制御クラス @n
+				※シングルトン @n
+				※内部低速オシレーターを使ったタイマー　@n
+				※設定周波数に大きな制限がある。
+		@param[in]	T	内部カウンターの
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	template <typename T>
+	class itimer {
+
+		static volatile T counter_;
+
+		uint8_t	intr_level_ = 0;
+
+		inline void wait_() const { asm("nop"); }
+
+	public:
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  インターバル・タイマー割り込みタスク
+		*/
+		//-----------------------------------------------------------------//
+		static __attribute__ ((interrupt)) void task() __attribute__ ((section (".lowtext")))
+		{
+			++counter_;
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  インターバル・タイマーを動作させる
+			@param[in]	freq	インターバル周波数
+			@param[in]	level	割り込みレベル（１～２）、０の場合はポーリング
+			@return エラーなら「false」
+		*/
+		//-----------------------------------------------------------------//
+		bool start(uint16_t freq, uint8_t level = 0)
+		{
+			intr_level_ = level;
+
+			// 低速発振器は１５ＫＨｚ
+			uint16_t v = 15000 / freq;
+			if(v > 4096 || v < 2) {
+				return false;
+			}
+
+			PER0.RTCEN = 1;
+
+			OSMC.WUTMMCK0 = 1;
+			ITMC = (v - 1) | 0x8000;
+
+			counter_ = 0;
+
+			// 割り込みレベル設定とマスク解除
+			if(level > 0) {
+				--level;
+				level ^= 0x03;
+				intr::PR01H.ITPR = (level) & 1;
+				intr::PR11H.ITPR = (level & 2) >> 1;
+				intr::MK1H.ITMK = 0;
+			}
+
+			return true;
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  同期回数を取得
+			@return 同期回数
+		*/
+		//-----------------------------------------------------------------//
+		T get_counter() const { return counter_; }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  インターバル・タイマーとの同期
+		*/
+		//-----------------------------------------------------------------//
+		void sync() const {
+			if(intr_level_) {
+				volatile T counter = counter_;
+				while(counter == counter_) wait_();
+			} else {
+				while(intr::IF1H.ITIF() == 0) wait_();
+				intr::IF1H.ITIF = 0;
+				++counter_;
+			}
+		}
+	};
+
+	template<typename T>
+		volatile T itimer<T>::counter_ = 0;
+
+}
