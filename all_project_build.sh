c24e1a09cbab00bd8ee44de2e86e060f0e804b38
hira <hira@rvf-rc45.net>
2016-07-25 18:49:01 +0900

    add shell

---
+++ all_project_build.sh
@@ -0,0 +1,20 @@
+cd FIRST_test
+make clean
+make
+cd ..
+cd INTERVAL_TIMER_test
+make clean
+make
+cd ..
+cd UART_test
+make clean
+make
+cd ..
+cd IICA_test
+make clean
+make
+cd ..
+cd DS3231_test
+make clean
+make
+cd ..
