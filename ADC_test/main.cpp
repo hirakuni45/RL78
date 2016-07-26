37ce5bafc1fc7ed8c078077fc63056c844c82e50
hira <hira@rvf-rc45.net>
2016-07-26 15:55:28 +0900

    update A/D project

--- ADC_test/main.cpp
+++ ADC_test/main.cpp
@@ -1,14 +1,16 @@
 //=====================================================================//
 /*!	@file
-	@brief	A/D の簡単なテスト
+	@brief	A/D の簡単なサンプル
 	@author	平松邦仁 (hira@rvf-rc45.net)
 */
 //=====================================================================//
 #include <cstdint>
 #include "G13/system.hpp"
 #include "G13/port.hpp"
+#include "common/fifo.hpp"
 #include "common/uart_io.hpp"
 #include "common/format.hpp"
+#include "common/itimer.hpp"
 #include "G13/adc.hpp"
 
 namespace {
@@ -17,9 +19,13 @@ namespace {
 		asm("nop");
 	}
 
-	device::uart_io<device::SAU00, device::SAU01, 128, 128> uart0_io_;
+	typedef utils::fifo<128> buffer;
+	device::uart_io<device::SAU00, device::SAU01, buffer, buffer> uart0_io_;
+
+	device::itimer<uint8_t> itm_;
 }
 
+/// 割り込みベクターの定義
 const void* ivec_[] __attribute__ ((section (".ivec"))) = {
 	/*  0 */  nullptr,
 	/*  1 */  nullptr,
@@ -37,6 +43,17 @@ const void* ivec_[] __attribute__ ((section (".ivec"))) = {
 	/* 13 */  reinterpret_cast<void*>(uart0_io_.send_task),
 	/* 14 */  reinterpret_cast<void*>(uart0_io_.recv_task),
 	/* 15 */  reinterpret_cast<void*>(uart0_io_.error_task),
+	/* 16 */  nullptr,
+	/* 17 */  nullptr,
+	/* 18 */  nullptr,
+	/* 19 */  nullptr,
+	/* 20 */  nullptr,
+	/* 21 */  nullptr,
+	/* 22 */  nullptr,
+	/* 23 */  nullptr,
+	/* 24 */  nullptr,
+	/* 25 */  nullptr,
+	/* 26 */  reinterpret_cast<void*>(itm_.task),
 };
 
 
@@ -57,23 +74,29 @@ int main(int argc, char* argv[])
 {
 	device::PM4.B3 = 0;  // output
 
-	uint8_t intr_level = 0;
-	uart0_io_.start(115200, intr_level);
+	{
+		uint8_t intr_level = 0;
+		uart0_io_.start(115200, intr_level);
+	}
 
-	uart0_io_.puts("Start RL78/G13 UART0 test...\n");
+	{
+		uint8_t intr_level = 1;
+		itm_.start(60, intr_level);
+	}
+
+	uart0_io_.puts("Start RL78/G13 A/D sample\n");
 
 	bool f = false;
 	uint32_t n = 0;
 	while(1) {
-		for(uint32_t i = 0; i < 100000; ++i) {
-			if(uart0_io_.recv_length()) {
-				auto ch = uart0_io_.getch();
-				if(ch == '\r') {
-					utils::format("%d\n") % n;
-					++n;
-				} else {
-					uart0_io_.putch(ch);
-				}
+		itm_.sync();
+		if(uart0_io_.recv_length()) {
+			auto ch = uart0_io_.getch();
+			if(ch == '\r') {
+				utils::format("%d\n") % n;
+				++n;
+			} else {
+				uart0_io_.putch(ch);
 			}
 		}
 		device::P4.B3 = f;
