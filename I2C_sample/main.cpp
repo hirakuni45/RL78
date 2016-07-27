cbadb79ef5ca06fd84b27fcf9f46b1d8424badcf
hira <hira@rvf-rc45.net>
2016-07-27 09:21:19 +0900

    update comment

--- I2C_sample/main.cpp
+++ I2C_sample/main.cpp
@@ -1,27 +1,31 @@
 //=====================================================================//
 /*!	@file
-	@brief	IICA(I2C) の簡単なテスト
+	@brief	IICA(I2C) のサンプル
 	@author	平松邦仁 (hira@rvf-rc45.net)
 */
 //=====================================================================//
 #include <cstdint>
 #include "G13/system.hpp"
 #include "G13/port.hpp"
+#include "common/itimer.hpp"
 #include "common/uart_io.hpp"
 #include "common/format.hpp"
 #include "common/iica_io.hpp"
 #include "common/fifo.hpp"
+#include "common/command.hpp"
 
 namespace {
 	void wait_()
 	{
 		asm("nop");
 	}
-	typedef device::iica_io<device::IICA0> IICA;
-	IICA iica0_io_;
+	device::itimer<uint8_t> itm_;
 
 	typedef utils::fifo<128> buffer;
 	device::uart_io<device::SAU00, device::SAU01, buffer, buffer> uart0_io_;
+
+	typedef device::iica_io<device::IICA0> IICA;
+	IICA iica0_io_;
 }
 
 const void* ivec_[] __attribute__ ((section (".ivec"))) = {
@@ -41,6 +45,17 @@ const void* ivec_[] __attribute__ ((section (".ivec"))) = {
 	/* 13 */  reinterpret_cast<void*>(uart0_io_.send_task),
 	/* 14 */  reinterpret_cast<void*>(uart0_io_.recv_task),
 	/* 15 */  reinterpret_cast<void*>(uart0_io_.error_task),
+	/* 16 */  nullptr,
+	/* 17 */  nullptr,
+	/* 18 */  nullptr,
+	/* 19 */  nullptr,
+	/* 20 */  nullptr,
+	/* 21 */  nullptr,
+	/* 22 */  nullptr,
+	/* 23 */  nullptr,
+	/* 24 */  nullptr,
+	/* 25 */  nullptr,
+	/* 26 */  reinterpret_cast<void*>(itm_.task),
 };
 
 extern "C" {
@@ -61,31 +76,19 @@ int main(int argc, char* argv[])
 	device::PM4.B3 = 0;  // output
 
 	{
+		uint8_t intr_level = 1;
+		uart0_io_.start(115200, intr_level);
+	}
+
+	{
 		uint8_t intr_level = 0;
 		iica0_io_.start(IICA::speed::fast, intr_level);
 	}
 
-	{
-		uint8_t intr_level = 1;
-		uart0_io_.start(115200, intr_level);
-	}
-	uart0_io_.puts("Start RL78/G13 I2C test...\n");
+	uart0_io_.puts("Start RL78/G13 I2C sample\n");
 
-	// I2C 送信
-	{
-		uint8_t tmp[4];
-		tmp[0] = 0x00;
-		tmp[1] = 0x01;
-		tmp[2] = 0x02;
-		tmp[3] = 0x03;
-		iica0_io_.send(0x71, tmp, 4);
-	}
 
-	// I2C 受信
-	{
-		uint8_t tmp[4];
-		iica0_io_.recv(0x71, tmp, 4);
-	}
+
 
 	bool f = false;
 	uint32_t n = 0;
