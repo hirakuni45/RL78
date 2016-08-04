1b49ebe7af47297d58d22c81fb69274f0465d7de
hira <hira@rvf-rc45.net>
2016-08-04 16:30:12 +0900

    fix read sync

--- common/csi_io.hpp
+++ common/csi_io.hpp
@@ -27,10 +27,66 @@ namespace device {
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 	template <class SAU>
 	class csi_io {
+	public:
 
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  データ、クロック位相タイプ
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		enum class PHASE : uint8_t {
+			TYPE1,  ///< タイプ１
+			TYPE2,  ///< タイプ２
+			TYPE3,  ///< タイプ３
+			TYPE4,  ///< タイプ４ (SD カードアクセス）
+		};
+
+	private:
 		uint8_t	intr_level_ = 0;
 
 		inline void sleep_() { asm("nop"); }
+
+		void sync_() {
+			if(SAU::get_unit_no() == 0) {
+				switch(SAU::get_chanel_no()) {
+				case 0:
+					while(intr::IF0H.CSIIF00() == 0) sleep_();
+					intr::IF0H.CSIIF00 = 0;
+					break;
+				case 1:
+					while(intr::IF0H.CSIIF01() == 0) sleep_();
+					intr::IF0H.CSIIF01 = 0;
+					break;
+				case 2:
+					while(intr::IF1L.CSIIF10() == 0) sleep_();
+					intr::IF1L.CSIIF10 = 0;
+					break;
+				case 3:
+					while(intr::IF1L.CSIIF11() == 0) sleep_();
+					intr::IF1L.CSIIF11 = 0;
+					break;
+				}
+			} else {
+				switch(SAU::get_chanel_no()) {
+				case 0:
+					while(intr::IF0H.CSIIF20() == 0) sleep_();
+					intr::IF0H.CSIIF20 = 0;
+					break;
+				case 1:
+					while(intr::IF0H.CSIIF21() == 0) sleep_();
+					intr::IF0H.CSIIF21 = 0;
+					break;
+				case 2:
+					while(intr::IF1H.CSIIF30() == 0) sleep_();
+					intr::IF1H.CSIIF30 = 0;
+					break;
+				case 3:
+					while(intr::IF1H.CSIIF31() == 0) sleep_();
+					intr::IF1H.CSIIF31 = 0;
+					break;
+				}
+			}
+		}
 
 	public:
 		// 割り込み設定
@@ -69,7 +125,8 @@ namespace device {
 			}
 		}
 
-		static __attribute__ ((interrupt)) void send_task() __attribute__ ((section (".lowtext")))
+
+		static __attribute__ ((interrupt)) void task() __attribute__ ((section (".lowtext")))
 		{
 #if 0
 			if(send_.length()) {
@@ -81,42 +138,35 @@ namespace device {
 #endif
 		}
 
-		static __attribute__ ((interrupt)) void recv_task() __attribute__ ((section (".lowtext")))
-		{
-////			recv_.put(rx_.SDR_L());
-		}
-
 
 		//-----------------------------------------------------------------//
 		/*!
 			@brief  通信速度を設定して、CSI を有効にする
 			@param[in]	speed	通信速度
+			@param[in]	dctype	データ、クロック位相タイプ
 			@param[in]	level	割り込みレベル（１～２）、０の場合はポーリング
-			@return エラーなら「false」
+			@return エラー（速度設定範囲外）なら「false」
 		*/
 		//-----------------------------------------------------------------//
-		bool start(uint32_t speed, uint8_t level = 0)
+		bool start(uint32_t speed, PHASE dctype, uint8_t level = 0)
 		{
 			intr_level_ = level;
 
-			SAU::SS = 0;	// disable
+			SAU::SS = 0;	// unit disable
+			SAU::SOE = 0;
+			interrupt_mask_(1);  // mask
 
 			// ボーレートから分周比の算出
 			if(speed == 0) {
 				return false;
 			}
+			if(SAU::get_unit_no() == 0 && SAU::get_chanel_no() == 0) {
+				if(speed > 16000000) return false;
+			} else {
+				if(speed > 8000000) return false;
+			}
 			auto div = static_cast<uint32_t>(F_CLK) / speed;
-			uint8_t master = 2;
-			if(SAU::get_unit_no() == 0 && SAU::get_chanel_no() == 0) {  // Max 16MHz
-				div /= 2;
-			} else {  // Max 8MHz
-				master = 4;
-				div /= 2;
-			}
-			if(div == 0) {
-				return false;
-			}
-
+			uint8_t master = 0;
 			while(div > 256) {
 				div /= 2;
 				++master;
@@ -145,32 +195,38 @@ namespace device {
 				cks = 1;
 			}
 
-			SAU::SMR = 0x0020 | SAU::SMR.CKS.b(cks) | SAU::SMR.MD0.b(1);
+			SAU::SMR = 0x0020 | SAU::SMR.CKS.b(cks);
 
 			// MSB first, 8 bits data
-			SAU::SCR = 0x0004 | SAU::SCR.TXE.b(1) | SAU::SCR.RXE.b(1) | SAU::SCR.DLS.b(3);
+			SAU::SCR = 0x0004 | SAU::SCR.TXE.b(1) | SAU::SCR.RXE.b(1) | SAU::SCR.DLS.b(3)
+				| SAU::SCR.DAP.b(static_cast<uint8_t>(dctype) >> 1)
+				| SAU::SCR.CKP.b(static_cast<uint8_t>(dctype) & 1);
 
 			// ボーレート・ジェネレーター設定
 			SAU::SDR = div << 8;
 
-			SAU::SOL = 0;	// 
-			SAU::CKO = 0;	// クロック出力設定
-			SAU::SO  = 0;	// シリアル出力設定
+			SAU::SOL = 0;
+			SAU::CKO = !SAU::SCR.CKP();	// クロック出力設定
+			SAU::SO  = 1;	// シリアル出力設定
 			SAU::SOE = 1;	// シリアル出力許可
 
 			// 対応するポートの設定
 			if(SAU::get_unit_no() == 0) {
 				if(SAU::get_chanel_no() == 0) {  // Chanel 0
-					PM1.B2 = 0;	// P1-2 output (SO00)
-					PM1.B1 = 1;	// P1-1 input  (SI00)
-					PM1.B0 = 0; // P1-0 output (SCK00)
-					P1.B2  = 1;	// ポートレジスター (SO00) 切り替え
-					P1.B1  = 1; // ポートレジスター (SI00) 切り替え
-					P1.B0  = 1; // ポートレジスター (SCK00) 切り替え
+					PM1.B2 = 0;	 // P1-2 output (SO00)
+					PM1.B1 = 1;	 // P1-1 input  (SI00)
+					PM1.B0 = 0;  // P1-0 output (SCK00)
+					PIM1.B1 = 1; // P1-1 TTL input
+					P1.B2  = 1;	 // ポートレジスター (SO00)  切り替え
+					P1.B1  = 1;  // ポートレジスター (SI00)  切り替え
+					P1.B0  = 1;  // ポートレジスター (SCK00) 切り替え
 				} else if(SAU::get_chanel_no() == 1) {  // Chanel 1
-
-
-
+					PM7.B3 = 0;  // P7-3 output (SO01)
+					PM7.B4 = 1;  // P7-4 input  (SI01)
+					PM7.B5 = 0;  // P7-5 output (SCK01)
+					P7.B3 = 1;   // ポートレジスター (SO01)  切り替え
+					P7.B4 = 1;   // ポートレジスター (SI01)  切り替え
+					P7.B5 = 1;   // ポートレジスター (SCK01) 切り替え
 				} else if(SAU::get_chanel_no() == 2) {  // Chanel 2
 					PM0.B2  = 0;  // P0-2 output (SO10)
 					PM0.B3  = 1;  // P0-3 input  (SI10)
@@ -178,26 +234,36 @@ namespace device {
 					PMC0.B2 = 0;  // ポートモードコントロール
 					PMC0.B3 = 0;  // ポートモードコントロール
 					PIM0.B3 = 1;  // ポート入力モード
-					P0.B2   = 1;  // ポートレジスター SO10 切り替え
-					P0.B3   = 1;  // ポートレジスター SI10 切り替え
+					P0.B2   = 1;  // ポートレジスター SO10  切り替え
+					P0.B3   = 1;  // ポートレジスター SI10  切り替え
 					P0.B4   = 1;  // ポートレジスター SCK10 切り替え
 				} else {  // chanel 3
-
-
-
+					PM5.B1 = 0;  // P5-1 output (SO11)
+					PM5.B0 = 1;  // P5-0 input  (SI11)
+					PM3.B0 = 0;  // P3-0 output (SCK11)
+					P5.B1 = 1;   // ポートレジスター SO11  切り替え
+					P5.B0 = 1;   // ポートレジスター SI11  切り替え
+					P3.B0 = 1;   // ポートレジスター SCK11 切り替え
 				}
 			} else {
 				if(SAU::get_chanel_no() == 0) {  // Chanel 0
-					PM1.B4 = 1;	// P1-4 input  (SI20)
-					PM1.B3 = 0;	// P1-3 output (SO20)
-					PM1.B5 = 0; // P1-5 output (SCK20)
-					P1.B3  = 1;	// ポートレジスター SO20 切り替え
-					P1.B5  = 1; // ポートレジスター SCK20 切り替え
-				} else {  // Chanel3（128ピンデバイスでサポート）
-					PM14.B3 = 1;  // P14-3 input (SI30)
-					PM14.B4 = 0;  // P14-4 output (SO30)
+					PM1.B3 = 0;	 // P1-3 output (SO20)
+					PM1.B4 = 1;	 // P1-4 input  (SI20)
+					PM1.B5 = 0;  // P1-5 output (SCK20)
+					P1.B3  = 1;	 // ポートレジスター SO20  切り替え
+					P1.B4  = 1;  // ポートレジスター SI20  切り替え
+					P1.B5  = 1;  // ポートレジスター SCK20 切り替え
+				} else if(SAU::get_chanel_no() == 1) {  // Chanel 1
+					PM7.B2 = 0;  // P7-2 output (SO21)
+					PM7.B1 = 1;  // P7-1 input  (SI21)
+					PM7.B0 = 0;  // P7-0 output (SCK21)
+					P7.B2  = 1;  // ポートレジスター SO21  切り替え
+					P7.B1  = 1;  // ポートレジスター SI21  切り替え
+					P7.B0  = 1;  // ポートレジスター SCK21 切り替え
+				} else if(SAU::get_chanel_no() == 2) {  // Chanel 2（128ピンデバイスでサポート）
 
-					P14.B4  = 1;  // ポートレジスター SO30 切り替え
+				} else {  // Chanel 3（128ピンデバイスでサポート）
+
 				}
 			}
 
@@ -266,8 +332,8 @@ namespace device {
 			if(intr_level_) {
 
 			} else {
-				while(SAU::SSR.TSF() != 0) sleep_();
 				SAU::SDR_L = ch;
+				sync_();
 			}
 		}
 
@@ -301,7 +367,7 @@ namespace device {
 				return 0;
 			} else {
 				SAU::SDR_L = 0xff;
-				while(SAU::SSR.BFF() == 0) sleep_();
+				sync_();
 				return SAU::SDR_L();
 			}
 		}
