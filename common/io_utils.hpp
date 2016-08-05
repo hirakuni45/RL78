e68613ed9578ccc7ed7e6b8ce4f9c787bf11b109
hira <hira@rvf-rc45.net>
2016-08-05 21:07:05 +0900

    fix bitpos enum class

--- common/io_utils.hpp
+++ common/io_utils.hpp
@@ -243,26 +243,51 @@ namespace device {
 
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 	/*!
-		@brief  Read/Write ビット・アクセス・テンプレート
-		@param[in]	T	アクセス・クラス
-		@param[in]	pos	初期位置
+		@brief  ビット位置定義
 	*/
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
-	template <class T, uint8_t pos>
+	enum class bitpos : uint8_t {
+		B0,		///< ビット０
+		B1,		///< ビット１
+		B2,		///< ビット２
+		B3,		///< ビット３
+		B4,		///< ビット４
+		B5,		///< ビット５
+		B6,		///< ビット６
+		B7,		///< ビット７
+		B8,		///< ビット８
+		B9,		///< ビット９
+		B10,	///< ビット１０
+		B11,	///< ビット１１
+		B12,	///< ビット１２
+		B13,	///< ビット１３
+		B14,	///< ビット１４
+		B15,	///< ビット１５
+	};
+
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief  Read/Write ビット・アクセス・テンプレート
+		@param[in]	T	アクセス・クラス
+		@param[in]	pos	ビット位置
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	template <class T, bitpos pos>
 	struct bit_rw_t {
 		static bool get() {
-			return (T::read() >> pos) & 1;
+			return (T::read() >> static_cast<uint8_t>(pos)) & 1;
 		}
 		static void set(bool v) {
 			if(v) {
-				T::write(T::read() | (static_cast<typename T::value_type>(v) << pos));
+				T::write(T::read() | (static_cast<typename T::value_type>(v) << static_cast<uint8_t>(pos)));
 			} else {
-				T::write(T::read() & ~(static_cast<typename T::value_type>(1) << pos));
+				T::write(T::read() & ~(static_cast<typename T::value_type>(1) << static_cast<uint8_t>(pos)));
 			}
 		}
 
 	    typename T::value_type b(bool v) const {
-			return v << pos;
+			return v << static_cast<uint8_t>(pos);
 		}
 
 		void operator = (bool v) const { set(v); }
@@ -274,22 +299,22 @@ namespace device {
 	/*!
 		@brief  Read/Write ビット・アクセス・テンプレート
 		@param[in]	T	アクセス・クラス
-		@param[in]	pos	初期位置
+		@param[in]	pos	初期ビット位置
 		@param[in]	len	ビット幅
 	*/
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
-	template <class T, uint8_t pos, uint8_t len>
+	template <class T, bitpos pos, uint8_t len>
 	struct bits_rw_t {
 		static typename T::value_type get() {
-			return (typename T::read() >> pos) & ((1 << len) - 1);
+			return (typename T::read() >> static_cast<uint8_t>(pos)) & ((1 << len) - 1);
 		}
 		static void set(typename T::value_type v) {
-			auto m = static_cast<typename T::value_type>(((1 << len) - 1) << pos);
-			T::write((T::read() & ~m) | (static_cast<typename T::value_type>(v) << pos));
+			auto m = static_cast<typename T::value_type>(((1 << len) - 1) << static_cast<uint8_t>(pos));
+			T::write((T::read() & ~m) | (static_cast<typename T::value_type>(v) << static_cast<uint8_t>(pos)));
 		}
 
 	    typename T::value_type b(typename T::value_type v) const {
-			return (((1 << len) - 1) & v) << pos;
+			return (((1 << len) - 1) & v) << static_cast<uint8_t>(pos);
 		}
 
 		void operator = (typename T::value_type v) const { set(v); }
@@ -301,18 +326,18 @@ namespace device {
 	/*!
 		@brief  Read ビット・アクセス・テンプレート
 		@param[in]	T	アクセス・クラス
-		@param[in]	pos	初期位置
+		@param[in]	pos	ビット位置
 		@param[in]	len	ビット幅
 	*/
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
-	template <class T, uint8_t pos>
+	template <class T, bitpos pos>
 	struct bit_ro_t {
 		static bool get() {
-			return (T::read() >> pos) & 1;
+			return (T::read() >> static_cast<uint8_t>(pos)) & 1;
 		}
 
 	    typename T::value_type b(typename T::value_type v) const {
-			return 1 << pos;
+			return 1 << static_cast<uint8_t>(pos);
 		}
 
 		bool operator () () const { return get(); }
@@ -333,13 +358,13 @@ namespace device {
 		using T::operator |=;
 		using T::operator &=;
 
-		bit_rw_t<T, 7> B7;	///< B7 アクセス
-		bit_rw_t<T, 6> B6;	///< B6 アクセス
-		bit_rw_t<T, 5> B5;	///< B5 アクセス
-		bit_rw_t<T, 4> B4;	///< B4 アクセス
-		bit_rw_t<T, 3> B3;	///< B3 アクセス
-		bit_rw_t<T, 2> B2;	///< B2 アクセス
-		bit_rw_t<T, 1> B1;	///< B1 アクセス
-		bit_rw_t<T, 0> B0;	///< B0 アクセス
+		bit_rw_t<T, bitpos::B7> B7;	///< B7 アクセス
+		bit_rw_t<T, bitpos::B6> B6;	///< B6 アクセス
+		bit_rw_t<T, bitpos::B5> B5;	///< B5 アクセス
+		bit_rw_t<T, bitpos::B4> B4;	///< B4 アクセス
+		bit_rw_t<T, bitpos::B3> B3;	///< B3 アクセス
+		bit_rw_t<T, bitpos::B2> B2;	///< B2 アクセス
+		bit_rw_t<T, bitpos::B1> B1;	///< B1 アクセス
+		bit_rw_t<T, bitpos::B0> B0;	///< B0 アクセス
 	};
 }
