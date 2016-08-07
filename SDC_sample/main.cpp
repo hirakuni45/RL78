b8f578d7575324c84cb66d8a34eb696c91fdc109
hira <hira@rvf-rc45.net>
2016-08-08 05:00:45 +0900

    strip value

--- SDC_sample/main.cpp
+++ SDC_sample/main.cpp
@@ -14,8 +14,7 @@
 #include "common/format.hpp"
 #include "common/delay.hpp"
 #include "common/csi_io.hpp"
-#include "ff12a/mmc_io.hpp"
-#include "ff12a/src/ff.h"
+#include "common/sdc_io.hpp"
 #include "common/command.hpp"
 
 namespace {
@@ -40,10 +39,8 @@ namespace {
 	typedef device::PORT<device::port_no::P0,  device::bitpos::B0> card_select;	///< カード選択信号
 	typedef device::PORT<device::port_no::P0,  device::bitpos::B1> card_power;	///< カード電源制御
 	typedef device::PORT<device::port_no::P14, device::bitpos::B6> card_detect;	///< カード検出
-	fatfs::mmc_io<csi, card_select> mmc_(csi_);
 
-	// FatFS コンテキスト
-	FATFS	fatfs_;
+	utils::sdc_io<csi, card_select, card_power, card_detect> sdc_(csi_);
 
 	utils::command<64> command_;
 }
@@ -100,24 +97,24 @@ extern "C" {
 		return uart_.recv_length();
 	}
 
-	DSTATUS disk_initialize (BYTE drv) {
-		return mmc_.disk_initialize(drv);
+	DSTATUS disk_initialize(BYTE drv) {
+		return sdc_.at_mmc().disk_initialize(drv);
 	}
 
 	DSTATUS disk_status(BYTE drv) {
-		return mmc_.disk_status(drv);
+		return sdc_.at_mmc().disk_status(drv);
 	}
 
 	DRESULT disk_read(BYTE drv, BYTE* buff, DWORD sector, UINT count) {
-		return mmc_.disk_read(drv, buff, sector, count);
+		return sdc_.at_mmc().disk_read(drv, buff, sector, count);
 	}
 
 	DRESULT disk_write(BYTE drv, const BYTE* buff, DWORD sector, UINT count) {
-		return mmc_.disk_write(drv, buff, sector, count);
+		return sdc_.at_mmc().disk_write(drv, buff, sector, count);
 	}
 
 	DRESULT disk_ioctl(BYTE drv, BYTE ctrl, void* buff) {
-		return mmc_.disk_ioctl(drv, ctrl, buff);
+		return sdc_.at_mmc().disk_ioctl(drv, ctrl, buff);
 	}
 
 	DWORD get_fattime(void) {
@@ -126,41 +123,6 @@ extern "C" {
 };
 
 namespace {
-	void list_dir_()
-	{
-		DIR dir;
-		auto st = f_opendir(&dir, "");
-		if(st != FR_OK) {
-			utils::format("Can't open dir: %d\n") % static_cast<uint32_t>(st);
-		} else {
-			for(;;) {
-				FILINFO fno;
-				// Read a directory item
-				if(f_readdir(&dir, &fno) != FR_OK) {
-					sci_puts("Can't read dir\n");
-					break;
-				}
-				if(!fno.fname[0]) break;
-					if(fno.fattrib & AM_DIR) {
-					utils::format("          /%s\n") % fno.fname;
-				} else {
-					utils::format("%8d  %s\n") % static_cast<uint32_t>(fno.fsize) % fno.fname;
-				}
-			}
-		}
-	}
-
-	// CSI 開始
-	// ※SD カードのアクセスでは、「PHASE::TYPE4」を選択する。
-	void start_csi_()
-	{
-		uint8_t intr_level = 0;
-		if(!csi_.start(16000000, csi::PHASE::TYPE4, intr_level)) {
-			uart_.puts("CSI Start fail ! (Clock spped over range)\n");
-		}
-	}
-
-
 	bool check_key_word_(uint8_t idx, const char* key)
 	{
 		char buff[12];
@@ -182,18 +144,6 @@ int main(int argc, char* argv[])
 
 	PM4.B3 = 0;  // output
 
-	card_select::PMC = 0;
-	card_select::PM = 0;
-	card_select::P = 0;  // 電源ＯＦＦ時、「０」にしておかないと電流が回り込む
-
-	card_power::PMC = 0;
-	card_power::PM = 0;
-	card_power::P = 1; 
-
-	card_detect::PM = 1;  // input
-	card_detect::POM = 0;
-	card_detect::PU = 1;  // pull-up
-
 	// インターバル・タイマー開始
 	{
 		uint8_t intr_level = 1;
@@ -206,57 +156,24 @@ int main(int argc, char* argv[])
 		uart_.start(115200, intr_level);
 	}
 
-	// CSI を初期化後、廃棄する事で関係ポートを初期化する。
-	{
-		start_csi_();
-		csi_.destroy();
-	}
+	sdc_.initialize();
 
 	uart_.puts("Start RL78/G13 SD-CARD Access sample\n");
 
 	command_.set_prompt("# ");
 
 	uint8_t n = 0;
-	bool cd = false;
-	uint8_t mount_delay = 0;
 	while(1) {
 		itm_.sync();
 
-		auto st = !card_detect::P();
-		if(!cd && st) {
-			start_csi_();
-			mount_delay = 10;
-			card_power::P = 0;
-			card_select::P = 1;
-//			utils::format("Card ditect\n");
-		} else if(cd && !st) {
-			f_mount(&fatfs_, "", 0);
-			csi_.destroy();
-			card_power::P = 1;
-			card_select::P = 0;
-//			utils::format("Card unditect\n");
-		}
-		cd = st;
-
-		if(mount_delay) {
-			--mount_delay;
-			if(mount_delay == 0) {
-				auto st = f_mount(&fatfs_, "", 1);
-				if(st != FR_OK) {
-					utils::format("Mount fail: %d\n") % static_cast<uint32_t>(st);
-					csi_.destroy();
-					card_power::P = 1;
-					card_select::P = 0;
-				}
-			}
-		}
+		sdc_.service();
 
 		// コマンド入力と、コマンド解析
 		if(command_.service()) {
 			auto cmdn = command_.get_words();
 			if(cmdn >= 1) {
 				if(check_key_word_(0, "dir")) {
-					list_dir_();
+					sdc_.dir("");
 				}
 			}
 		}
