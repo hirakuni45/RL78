dd523edb9e165e0ff4e5c36f4a4a8273987811b7
Kunihito Hiramatsu <hira@rvf-rc45.net>
2016-08-26 23:41:01 +0900

    fix CR/LF to LF

--- all_project_build.sh
+++ all_project_build.sh
@@ -1,11 +1,13 @@
-for file in `ls -d *_sample`; do
+#!/bin/sh                                                                                       
+for file in `ls -d *_sample`
+do
     cd "${file}"
-	make clean
-	make
-#	echo $?
-	if [ $? -ne 0 ]; then
-		echo "Compile error: " ${file}
-		break;
-	fi
-	cd ..
+    make clean
+    make
+#   echo $?                                                                                     
+    if [ $? -ne 0 ]; then
+        echo "Compile error: " ${file}
+        break;
+    fi
+    cd ..
 done
