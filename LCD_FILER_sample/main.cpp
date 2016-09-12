988b10ee5bb20843c8da78c1a5c35aadf4a171fb
hira <hira@rvf-rc45.net>
2016-09-12 10:32:48 +0900

    update kanji output test

--- LCD_FILER_sample/main.cpp
+++ LCD_FILER_sample/main.cpp
@@ -41,38 +41,36 @@ namespace {
 
 	device::itimer<uint8_t> itm_;
 
-	// CSI(SPI) の定義、CSI20 の通信では、「SAU10」を利用、１ユニット、チャネル０
-	typedef device::csi_io<device::SAU10> csi;
+	// SDC CSI(SPI) の定義、CSI00 の通信では、「SAU00」を利用、０ユニット、チャネル０
+	typedef device::csi_io<device::SAU00> csi;
 	csi csi_;
+
+	// FatFS インターフェースの定義
+	typedef device::PORT<device::port_no::P0,  device::bitpos::B0> card_select;	///< カード選択信号
+	typedef device::PORT<device::port_no::P0,  device::bitpos::B1> card_power;	///< カード電源制御
+	typedef device::PORT<device::port_no::P14, device::bitpos::B6> card_detect;	///< カード検出
+
+	utils::sdc_io<csi, card_select, card_power, card_detect> sdc_(csi_);
+
+	// LCD CSI(SPI) の定義、CSI20 の通信では、「SAU10」を利用、１ユニット、チャネル０
+	typedef device::csi_io<device::SAU10> csig;
+	csig csig_;
 
 	// LCD インターフェースの定義
 	typedef device::PORT<device::port_no::P5,  device::bitpos::B5> lcd_sel;	///< LCD 選択信号
 	typedef device::PORT<device::port_no::P5,  device::bitpos::B0> lcd_reg;	///< LCD レジスタ選択
 
 #ifdef LCD_ST7565
-	chip::ST7565<csi, lcd_sel, lcd_reg> lcd_(csi_);
+	chip::ST7565<csig, lcd_sel, lcd_reg> lcd_(csig_);
 #endif
 #ifdef LCD_SSD1306
-	chip::SSD1306<csi, lcd_sel, lcd_reg> lcd_(csi_);
+	chip::SSD1306<csig, lcd_sel, lcd_reg> lcd_(csig_);
 #endif
 
-	typedef graphics::font6x12 ascii_font;
-//	graphics::monograph<128, 64, ascii_font> bitmap_;
-	graphics::monograph<128, 64> bitmap_;
-
-	uint8_t v_ = 91;
-	uint8_t m_ = 123;
-	uint8_t rand_()
-	{
-		v_ += v_ << 2;
-		++v_;
-		uint8_t n = 0;
-		if(m_ & 0x02) n = 1;
-		if(m_ & 0x40) n ^= 1;
-		m_ += m_;
-		if(n == 0) ++m_;
-		return v_ ^ m_;
-	}
+	typedef graphics::font6x12 afont;
+	typedef graphics::kfont12<16> kfont;
+	kfont kfont_;
+	graphics::monograph<128, 64, afont, kfont> bitmap_(kfont_);
 }
 
 const void* ivec_[] __attribute__ ((section (".ivec"))) = {
@@ -116,8 +114,39 @@ extern "C" {
 	{
 		uart_.puts(str);
 	}
-};
 
+	DSTATUS disk_initialize(BYTE drv) {
+		return sdc_.at_mmc().disk_initialize(drv);
+	}
+
+	DSTATUS disk_status(BYTE drv) {
+		return sdc_.at_mmc().disk_status(drv);
+	}
+
+	DRESULT disk_read(BYTE drv, BYTE* buff, DWORD sector, UINT count) {
+		return sdc_.at_mmc().disk_read(drv, buff, sector, count);
+	}
+
+	DRESULT disk_write(BYTE drv, const BYTE* buff, DWORD sector, UINT count) {
+		return sdc_.at_mmc().disk_write(drv, buff, sector, count);
+	}
+
+	DRESULT disk_ioctl(BYTE drv, BYTE ctrl, void* buff) {
+		return sdc_.at_mmc().disk_ioctl(drv, ctrl, buff);
+	}
+
+	DWORD get_fattime(void) {
+#ifdef WITH_RTC
+		time_t t = 0;
+		if(!rtc_.get_time(t)) {
+			utils::format("Stall RTC read (%d)\n") % static_cast<uint32_t>(iica_.get_last_error());
+		}
+		return utils::str::get_fattime(t);
+#else
+		return 0;
+#endif
+	}
+};
 
 
 int main(int argc, char* argv[])
@@ -138,15 +167,18 @@ int main(int argc, char* argv[])
 		uart_.start(115200, intr_level);
 	}
 
-	// CSI 開始
+	// SD カードアクセス開始
+	sdc_.initialize();
+
+	// CSI graphics 開始
 	{
 		uint8_t intr_level = 0;
-		if(!csi_.start(8000000, csi::PHASE::TYPE4, intr_level)) {
+		if(!csig_.start(8000000, csig::PHASE::TYPE4, intr_level)) {
 			uart_.puts("CSI Start fail...\n");
 		}
 	}
 
-	uart_.puts("Start RL78/G13 ST7565(R) sample\n");
+	uart_.puts("Start RL78/G13 SDC Filer sample\n");
 
 	PM4.B3 = 0;  // output
 
@@ -155,37 +187,22 @@ int main(int argc, char* argv[])
 		bitmap_.flash(0);
 	}
 
-	uint16_t x = rand_() & 127;
-	uint16_t y = rand_() & 63;
-	uint16_t xx;
-	uint16_t yy;
-	uint8_t loop = 0;
-	uint8_t	nn = 0;
-
 	uint8_t n = 0;
+	uint8_t nn = 0;
+
 	while(1) {
 		itm_.sync();
 
-		if(nn >= 4) {
+		sdc_.service();
+
+		if(nn >= 3) {
 			lcd_.copy(bitmap_.fb());
+			bitmap_.flash(0);
 			nn = 0;
 		}
 		++nn;
 
-		if(loop >= 20) {
-			loop = 0;
-			bitmap_.flash(0);
-///			bitmap_.frame(0, 0, 128, 64, 1);
-		}
-#if 0
-		xx = rand_() & 127;
-		yy = rand_() & 63;
-		bitmap_.line(x, y, xx, yy, 1);
-		x = xx;
-		y = yy;
-#endif
-		bitmap_.draw_text(0, 0, "ABCDEFG");
-		++loop;
+		bitmap_.draw_text(0, 0, "漢字");
 
 		++n;
 		if(n >= 30) n = 0;
