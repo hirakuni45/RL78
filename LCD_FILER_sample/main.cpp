3e991f7d5c6faed26cb5c1ac9700c9dfa0ec73d3
hira <hira@rvf-rc45.net>
2016-09-18 17:48:30 +0900

    fix operations

--- LCD_FILER_sample/main.cpp
+++ LCD_FILER_sample/main.cpp
@@ -16,6 +16,7 @@
 #include "common/fifo.hpp"
 #include "common/uart_io.hpp"
 #include "common/itimer.hpp"
+#include "common/adc_io.hpp"
 #include "common/format.hpp"
 #include "common/delay.hpp"
 #include "common/monograph.hpp"
@@ -23,6 +24,8 @@
 #include "common/kfont12.hpp"
 #include "common/sdc_io.hpp"
 #include "common/filer.hpp"
+#include "common/bitset.hpp"
+#include "common/switch_man.hpp"
 
 // ターゲットＬＣＤのタイプを選択
 #define LCD_ST7565
@@ -34,13 +37,32 @@
 #include "chip/SSD1306.hpp"
 #endif
 
+// スイッチ入力を有効にする場合
+// ※スイッチの接続は、ADC_SWITCH_sample を参照
+#define ADC_SWITCH
+
 namespace {
+
 	// 送信、受信バッファの定義
 	typedef utils::fifo<uint8_t, 32> buffer;
 	// UART の定義（SAU02、SAU03）
 	device::uart_io<device::SAU02, device::SAU03, buffer, buffer> uart_;
 
 	device::itimer<uint8_t> itm_;
+
+#ifdef ADC_SWITCH
+	enum class SWITCH : uint8_t {
+		RIGHT,
+		UP,
+		DOWN,
+		LEFT,
+		A,
+		B
+	};
+
+	typedef utils::bitset<uint8_t, SWITCH> switch_bits;
+	utils::switch_man<switch_bits> switch_man_;
+#endif
 
 	// SDC CSI(SPI) の定義、CSI00 の通信では、「SAU00」を利用、０ユニット、チャネル０
 	typedef device::csi_io<device::SAU00> csi;
@@ -74,6 +96,9 @@ namespace {
 	kfont kfont_;
 	typedef graphics::monograph<128, 64, afont, kfont> bitmap;
 	bitmap bitmap_(kfont_);
+
+	typedef device::adc_io<utils::null_task> adc;
+	adc adc_;
 
 	graphics::filer<sdc_io, bitmap> filer_(sdc_, bitmap_);
 }
@@ -170,9 +195,11 @@ int main(int argc, char* argv[])
 
 	utils::port::pullup_all();  ///< 安全の為、全ての入力をプルアップ
 
+	PM4.B3 = 0;  // output
+
 	// インターバル・タイマー開始
 	{
-		uint8_t intr_level = 1;
+		uint8_t intr_level = 2;
 		itm_.start(60, intr_level);
 	}
 
@@ -180,6 +207,14 @@ int main(int argc, char* argv[])
 	{
 		uint8_t intr_level = 1;
 		uart_.start(115200, intr_level);
+	}
+
+	// A/D 開始
+	{
+		device::PM2.B2 = 1;
+		device::PM2.B3 = 1;
+		uint8_t intr_level = 0;
+		adc_.start(adc::REFP::VDD, adc::REFM::VSS, intr_level);
 	}
 
 	// SD カードアクセス開始
@@ -195,8 +230,6 @@ int main(int argc, char* argv[])
 
 	uart_.puts("Start RL78/G13 SDC Filer sample\n");
 
-	PM4.B3 = 0;  // output
-
 	{
 		lcd_.start(0x04, true);
 		bitmap_.flash(0);
@@ -210,6 +243,33 @@ int main(int argc, char* argv[])
 	while(1) {
 		itm_.sync();
 
+#ifdef ADC_SWITCH
+		switch_bits lvl;
+		// ４つのスイッチ判定（排他的）
+		auto val = adc_.get(2);
+		val >>= 6;   // 0 to 1023
+		val += 128;  // 閾値のオフセット（1024 / 4(SWITCH) / 2）
+		val /= 256;  // デコード（1024 / 4(SWITCH）
+
+		if(val < 4) {
+			lvl.set(static_cast<SWITCH>(val));
+		}
+
+		// ２つのスイッチ判定（同時押し判定）
+		val = adc_.get(3);
+		val >>= 6;  // 0 to 1023
+		if(val < 256) {
+			lvl.set(SWITCH::A);
+			lvl.set(SWITCH::B);
+		} else if(val < 594) {
+			lvl.set(SWITCH::A);
+		} else if(val < 722) {
+			lvl.set(SWITCH::B);
+		}
+
+		switch_man_.service(lvl);
+#endif
+
 		fbf_back = fbf;
 		if(fbf) {
 			bitmap_.flash(0);
@@ -221,36 +281,50 @@ int main(int argc, char* argv[])
 
 		fbcopy = filer_.service(f);
 
+		char ch = 0;
 		if(sci_length()) {
-			auto ch = sci_getch();
-			if(ch == ' ') {
-				if(filer_.ready()) {
-					fbf = filer_.start();
-				}
-			} else if(ch == 'a') {
-				if(!filer_.ready()) {
-					fbf = filer_.set_focus(-1);
-				}
-			} else if(ch == 'z') {
-				if(!filer_.ready()) {
-					fbf = filer_.set_focus(1);
-				}
-			} else if(ch == '\r') {
-				if(!filer_.ready()) {
-					if(filer_.set_directory()) {
+			ch = sci_getch();
+		}
+
+#ifdef ADC_SWITCH
+		if(switch_man_.get_positive().get(SWITCH::A)) ch = ' ';
+		else if(switch_man_.get_positive().get(SWITCH::UP)) ch = 'a';
+		else if(switch_man_.get_positive().get(SWITCH::DOWN)) ch = 'z';
+		else if(switch_man_.get_positive().get(SWITCH::RIGHT)) ch = '\r';
+		else if(switch_man_.get_positive().get(SWITCH::LEFT)) ch = 0x08;
+#endif
+		if(ch == ' ') {
+			if(filer_.ready()) {
+				fbf = filer_.start();
+			} else {
+				filer_.close();
+				fbf = true;
+			}
+		} else if(ch == 'a') {
+			if(!filer_.ready()) {
+				fbf = filer_.set_focus(-1);
+			}
+		} else if(ch == 'z') {
+			if(!filer_.ready()) {
+				fbf = filer_.set_focus(1);
+			}
+		} else if(ch == '\r') {
+			if(!filer_.ready()) {
+				if(filer_.set_directory()) {
+					fbf = true;
+				} else {
+					const char* p = filer_.get_select_path();
+					if(p != nullptr) {
+						utils::format("Path: '%s'\n") % p;
+						filer_.close();
 						fbf = true;
-					} else {
-						const char* p = filer_.get_select_path();
-						if(p != nullptr) {
-							utils::format("Path: '%s'\n") % p;
-						}
 					}
 				}
-			} else if(ch == 0x08) {
-				if(!filer_.ready()) {
-					if(filer_.set_directory(false)) {
-						fbf = true;
-					}
+			}
+		} else if(ch == 0x08) {
+			if(!filer_.ready()) {
+				if(filer_.set_directory(false)) {
+					fbf = true;
 				}
 			}
 		}
