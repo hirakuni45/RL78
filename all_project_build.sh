6395f442ba58dfece0b83d49ac49afbfef6d9c61
hira <hira@rvf-rc45.net>
2016-09-22 10:02:05 +0900

    update shell

--- all_project_build.sh
+++ all_project_build.sh
@@ -1,13 +1,30 @@
-#!/bin/sh                                                                                       
-for file in `ls -d *_sample`
+#!/bin/sh
+CMDNAME=`basename $0`
+if [[ $1 = "help" ]]; then
+  echo "Usage: $CMDNAME [clean]"
+  echo ""
+  exit
+fi
+
+# 本体
+for file in `ls -d *`
 do
+  if [ -e "${file}/Makefile" ]; then
+	echo -e "\e[1;35mStart project: " ${file} "\e[m"
     cd "${file}"
-    make clean
+    if [[ $1 = "clean" ]]; then
+      make clean
+      echo -e "\e[1;32mClean project: " ${file} "\e[m"
+    fi
     make
 #   echo $?                                                                                     
     if [ $? -ne 0 ]; then
-        echo "Compile error: " ${file}
+        echo -e "\e[1;31mCompile error: " ${file} "\e[m"
+		echo ""
         break;
     fi
     cd ..
+	echo -e "\e[1;36mEnd project: " ${file} "\e[m"
+    echo ""
+  fi
 done
