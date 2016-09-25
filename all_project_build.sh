1c590382c3c4394b9aaf5cb64240f64911d376b4
hira <hira@rvf-rc45.net>
2016-09-25 17:27:04 +0900

    update

--- all_project_build.sh
+++ all_project_build.sh
@@ -6,25 +6,37 @@ if [[ $1 = "help" ]]; then
   exit
 fi
 
-# 本体
+# make clean
+if [[ $1 = "clean" ]]; then
+  for file in `ls -d *`
+  do
+    if [ -e "${file}/Makefile" ]; then
+      cd "${file}"
+      echo -e "\e[1;32mClean project: " ${file} "\e[m"
+      make clean >& /dev/null
+      if [ $? -ne 0 ]; then
+          echo -e "\e[1;31mError: " ${file} "\e[m"
+          echo ""
+          break;
+      fi
+	  cd ..
+    fi
+  done
+fi
+
+# make
 for file in `ls -d *`
 do
   if [ -e "${file}/Makefile" ]; then
 	echo -e "\e[1;35mStart project: " ${file} "\e[m"
     cd "${file}"
-    if [[ $1 = "clean" ]]; then
-      make clean
-      echo -e "\e[1;32mClean project: " ${file} "\e[m"
-    fi
-    make
-#   echo $?                                                                                     
+    make > /dev/null
     if [ $? -ne 0 ]; then
         echo -e "\e[1;31mCompile error: " ${file} "\e[m"
 		echo ""
         break;
     fi
     cd ..
-	echo -e "\e[1;36mEnd project: " ${file} "\e[m"
-    echo ""
+	echo -e "\e[1;36mBuild project: " ${file} "\e[m"
   fi
 done
