7ff7f1eec45d6fb6084f141351a2417dd1eb5033
hira <hira@rvf-rc45.net>
2016-09-29 00:17:21 +0900

    fix echo color

--- all_project_build.sh
+++ all_project_build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 CMDNAME=`basename $0`
 if [[ $1 = "help" ]]; then
   echo "Usage: $CMDNAME [clean]"
@@ -6,16 +6,22 @@ if [[ $1 = "help" ]]; then
   exit
 fi
 
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+PINK=`tput setaf 5`
+LIGHTBLUE=`tput setaf 6`
+NOCOLOR=`tput sgr0`
+
 # make clean
 if [[ $1 = "clean" ]]; then
   for file in `ls -d *`
   do
     if [ -e "${file}/Makefile" ]; then
       cd "${file}"
-      echo "\033[1;32mClean project: " ${file} "\033[m"
+      echo "${GREEN}Clean project: " ${file} "${NOCOLOR}"
       make clean >& /dev/null
       if [ $? -ne 0 ]; then
-          echo "\033[1;31mError: " ${file} "\033[m"
+          echo "${RED}Error: " ${file} "${NOCOLOR}"
           echo ""
           break;
       fi
@@ -28,15 +34,15 @@ fi
 for file in `ls -d *`
 do
   if [ -e "${file}/Makefile" ]; then
-	echo "\033[1;35mStart project: " ${file} "\033[m"
+	echo "${PINK}Start project: " ${file} "${NOCOLOR}"
     cd "${file}"
     make > /dev/null
     if [ $? -ne 0 ]; then
-        echo "\033[1;31mCompile error: " ${file} "\033[m"
+        echo "${RED}Compile error: " ${file} "${NOCOLOR}"
 		echo ""
         break;
     fi
     cd ..
-	echo "\033[1;36mBuild project: " ${file} "\033[m"
+	echo "${LIGHTBLUE}Build project: " ${file} "${NOCOLOR}"
   fi
 done
