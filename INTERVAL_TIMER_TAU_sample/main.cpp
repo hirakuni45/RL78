a78864cb1e1822167d31dfaa962c54397ca30cd4
hira <hira@rvf-rc45.net>
2017-09-21 22:24:28 +0900

    update copyright text

--- INTERVAL_TIMER_TAU_sample/main.cpp
+++ INTERVAL_TIMER_TAU_sample/main.cpp
@@ -2,7 +2,10 @@
 /*!	@file
 	@brief	インターバル。タイマー出力サンプル @n
 			TO01(P1-6) に５ＫＨｚの矩形波を出力（１０ＫＨｚインターバル）
-	@author	平松邦仁 (hira@rvf-rc45.net)
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2016 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/RL78/blob/master/LICENSE
 */
 //=====================================================================//
 #include <cstdint>
