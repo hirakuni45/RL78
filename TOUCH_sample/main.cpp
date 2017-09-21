d89ddb01d3c6279c72f86ebc27a8a7908090c1a3
hira <hira@rvf-rc45.net>
2017-09-21 22:28:22 +0900

    update copyright text

--- TOUCH_sample/main.cpp
+++ TOUCH_sample/main.cpp
@@ -4,7 +4,10 @@
 			・プルアップ抵抗は１Ｍオーム @n
 			・タッチパッドは、１０ｍｍ四方 @n
 			・タッチパッドは、ポリイミドテープなどで絶縁する
-	@author	平松邦仁 (hira@rvf-rc45.net)
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2016 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/RL78/blob/master/LICENSE
 */
 //=====================================================================//
 #include "G13/system.hpp"
