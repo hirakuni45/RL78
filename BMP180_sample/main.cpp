a806113c8a304eee610019e0ba100b479957a2d7
hira <hira@rvf-rc45.net>
2017-09-21 22:20:49 +0900

    update copyright text

--- BMP180_sample/main.cpp
+++ BMP180_sample/main.cpp
@@ -1,7 +1,10 @@
 //=====================================================================//
 /*!	@file
 	@brief	BMP180/BMP280 温度、圧力センサのサンプル
-	@author	平松邦仁 (hira@rvf-rc45.net)
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2016 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/RL78/blob/master/LICENSE
 */
 //=====================================================================//
 #include <cstdint>
