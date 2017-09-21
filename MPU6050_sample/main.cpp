0dd1b6dad6c2876ee3ba4ae975d8884121b0b4e9
hira <hira@rvf-rc45.net>
2017-09-21 22:25:56 +0900

    update copyright text

--- MPU6050_sample/main.cpp
+++ MPU6050_sample/main.cpp
@@ -1,7 +1,10 @@
 //=====================================================================//
 /*!	@file
 	@brief	MPU6050 のサンプル
-	@author	平松邦仁 (hira@rvf-rc45.net)
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2016 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/RL78/blob/master/LICENSE
 */
 //=====================================================================//
 #include <cstdint>
