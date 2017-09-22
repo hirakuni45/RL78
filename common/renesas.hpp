ec50da17ecee5b76a19c46b1930ab2eaa5f83977
hira <hira@rvf-rc45.net>
2017-09-22 15:04:26 +0900

    new file

---
+++ common/renesas.hpp
@@ -0,0 +1,27 @@
+#pragma once
+//=====================================================================//
+/*!	@file
+	@brief	ルネサス RL78 選択
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2017 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/RL78/blob/master/LICENSE
+*/
+//=====================================================================//
+#include "common/vect.h"
+#include "common/delay.hpp"
+
+#if defined(SIG_G13)
+#include "G13/port.hpp"
+#include "G13/system.hpp"
+#include "G13/adc.hpp"
+#include "G13/dma.hpp"
+#include "G13/iica.hpp"
+#include "G13/intr.hpp"
+#include "G13/sau.hpp"
+#include "G13/tau.hpp"
+#include "G13/timer.hpp"
+
+#else
+#  error "Requires SIG_XXX to be defined"
+#endif
