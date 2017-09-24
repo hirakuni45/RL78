a1acc8f47cf5ab9b28bf261084a0c7bde79cce4c
hira <hira@rvf-rc45.net>
2017-09-25 00:17:34 +0900

    update interrupt handler

--- WAV_PLAYER_sample/main.cpp
+++ WAV_PLAYER_sample/main.cpp
@@ -10,6 +10,7 @@
 */
 //=====================================================================//
 #include <cstdint>
+#include "common/renesas.hpp"
 #include "common/port_utils.hpp"
 #include "common/tau_io.hpp"
 #include "common/fifo.hpp"
@@ -223,64 +224,6 @@ namespace {
 
 }
 
-#pragma GCC optimize ("O0")
-const void* ivec_[] __attribute__ ((section (".ivec"))) = {
-	/*  0 WDTI            */  nullptr,
-	/*  1 LVI             */  nullptr,
-	/*  2 P0              */  nullptr,
-	/*  3 P1              */  nullptr,
-	/*  4 P2              */  nullptr,
-	/*  5 P3              */  nullptr,
-	/*  6 P4              */  nullptr,
-	/*  7 P5              */  nullptr,
-	/*  8 ST2/CSI20/IIC20 */  nullptr,
-	/*  9 SR2/CSI21/IIC21 */  nullptr,
-	/* 10 SRE2/TM11H      */  nullptr,
-	/* 11 DMA0            */  nullptr,
-	/* 12 DMA1            */  nullptr,
-	/* 13 ST0/CSI00/IIC00 */  nullptr,
-	/* 14 SR0/CSI01/IIC01 */  nullptr,
-	/* 15 SRE0/TM01H      */  nullptr,
-	/* 16 ST1/CSI10/IIC10 */  reinterpret_cast<void*>(uart_.send_task),
-	/* 17 SR1/CSI11/IIC11 */  reinterpret_cast<void*>(uart_.recv_task),
-	/* 18 SRE1/TM03H      */  reinterpret_cast<void*>(uart_.error_task),
-	/* 19 IICA0           */  nullptr,
-	/* 20 TM00            */  reinterpret_cast<void*>(master_.task),
-	/* 21 TM01            */  nullptr,
-	/* 22 TM02            */  nullptr,
-	/* 23 TM03            */  nullptr,
-	/* 24 AD              */  reinterpret_cast<void*>(adc_.task),
-	/* 25 RTC             */  nullptr,
-	/* 26 IT              */  reinterpret_cast<void*>(itm_.task),
-	/* 27 KR              */  nullptr,
-	/* 28 ST3/CSI30/IIC30 */  nullptr,
-	/* 29 SR3/CSI31/IIC31 */  nullptr,
-	/* 30 TM13            */  nullptr,
-	/* 31 TM04            */  nullptr,
-	/* 32 TM05            */  nullptr,
-	/* 33 TM06            */  nullptr,
-	/* 34 TM07            */  nullptr,
-	/* 35 P6              */  nullptr,
-	/* 36 P7              */  nullptr,
-	/* 37 P8              */  nullptr,
-	/* 38 P9              */  nullptr,
-	/* 39 P10             */  nullptr,
-	/* 40 P11             */  nullptr,
-	/* 41 TM10            */  nullptr,
-	/* 42 TM11            */  nullptr,
-	/* 43 TM12            */  nullptr,
-	/* 44 SRE3/TM13H      */  nullptr,
-	/* 45 MD              */  nullptr,
-	/* 46 IICA1           */  nullptr,
-	/* 47 FL              */  nullptr,
-	/* 48 DMA2            */  nullptr,
-	/* 49 DMA3            */  nullptr,
-	/* 50 TM14            */  nullptr,
-	/* 51 TM15            */  nullptr,
-	/* 52 TM16            */  nullptr,
-	/* 53 TM17            */  nullptr,
-};
-#pragma GCC optimize ("Os")
 
 extern "C" {
 
@@ -311,6 +254,7 @@ extern "C" {
 	}
 #endif
 
+
 	void sci_putch(char ch)
 	{
 #ifdef ENABLE_LCD
@@ -323,6 +267,7 @@ extern "C" {
 		uart_.putch(ch);
 #endif
 	}
+
 
 	void sci_puts(const char* str)
 	{
@@ -337,38 +282,82 @@ extern "C" {
 #endif
 	}
 
+
 	char sci_getch(void)
 	{
 		return uart_.getch();
 	}
+
 
 	uint16_t sci_length()
 	{
 		return uart_.recv_length();
 	}
 
+
 	DSTATUS disk_initialize(BYTE drv) {
 		return sdc_.at_mmc().disk_initialize(drv);
 	}
+
 
 	DSTATUS disk_status(BYTE drv) {
 		return sdc_.at_mmc().disk_status(drv);
 	}
 
+
 	DRESULT disk_read(BYTE drv, BYTE* buff, DWORD sector, UINT count) {
 		return sdc_.at_mmc().disk_read(drv, buff, sector, count);
 	}
+
 
 	DRESULT disk_write(BYTE drv, const BYTE* buff, DWORD sector, UINT count) {
 		return sdc_.at_mmc().disk_write(drv, buff, sector, count);
 	}
 
+
 	DRESULT disk_ioctl(BYTE drv, BYTE ctrl, void* buff) {
 		return sdc_.at_mmc().disk_ioctl(drv, ctrl, buff);
 	}
 
+
 	DWORD get_fattime(void) {
 		return 0;
+	}
+
+
+	void UART1_TX_intr(void)
+	{
+		uart_.send_task();
+	}
+
+
+	void UART1_RX_intr(void)
+	{
+		uart_.recv_task();
+	}
+
+
+	void UART1_ER_intr(void)
+	{
+		uart_.error_task();
+	}
+
+
+	void ADC_intr(void)
+	{
+		adc_.task();
+	}
+
+
+	void ITM_intr(void)
+	{
+		itm_.task();
+	}
+
+
+	void TM00_intr(void)
+	{
+		master_.task();
 	}
 };
 
@@ -478,7 +467,7 @@ namespace {
 			% static_cast<uint32_t>(wav_.get_chanel())
 			% wav_.get_rate();
 		turn_bmp_ = false;
-		lcd_.copy(bitmap_.fb());
+		lcd_.copy(bitmap_.fb(), bitmap_.page_num());
 #endif
 
 		master_.at_task().set_rate(wav_.get_rate());
@@ -703,7 +692,7 @@ int main(int argc, char* argv[])
 		adc_.start_scan(2);
 
 		if((fbf_back && !fbf) || fbcopy) {
-			lcd_.copy(bitmap_.fb());
+			lcd_.copy(bitmap_.fb(), bitmap_.page_num());
 		}
 
 		adc_.sync();
