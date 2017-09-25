2a4f4b7615ae64b857d911663340766841bba7ec
hira <hira@rvf-rc45.net>
2017-09-26 05:23:22 +0900

    update copyright text

--- WAV_PLAYER_sample/wav_in.hpp
+++ WAV_PLAYER_sample/wav_in.hpp
@@ -1,7 +1,10 @@
 //=====================================================================//
 /*!	@file
 	@brief	WAV 音声ファイルを扱うクラス
-	@author	平松邦仁 (hira@rvf-rc45.net)
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2016 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/RL78/blob/master/LICENSE
 */
 //=====================================================================//
 #include <cstring>
