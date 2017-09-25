a9160c3b62285fc406d498892ea39127940a50d5
hira <hira@rvf-rc45.net>
2017-09-26 08:34:03 +0900

    update comment

--- common/delay.hpp
+++ common/delay.hpp
@@ -3,12 +3,17 @@
 /*!	@file
 	@brief	delay ユーティリティー (RL78/G13 32MHz)
     @author 平松邦仁 (hira@rvf-rc45.net)
-	@copyright	Copyright (C) 2016 Kunihito Hiramatsu @n
+	@copyright	Copyright (C) 2016, 2017 Kunihito Hiramatsu @n
 				Released under the MIT license @n
 				https://github.com/hirakuni45/RL78/blob/master/LICENSE
 */
 //=====================================================================//
 #include <cstdint>
+
+/// F_CLK が３２以外はエラーにする
+#if (F_CLK != 32000000)
+#  error "delay.hpp requires F_CLK to be defined"
+#endif
 
 namespace utils {
 
@@ -16,7 +21,11 @@ namespace utils {
 	/*!
 		@brief  基準クロック３２ＭＨｚの待ち @n
 				※-O2、-Os などの最適化が条件、最適化無しの場合などは、@n
-				アセンブルリストを参照の上、マシンサイクルを確認
+				アセンブルリストを参照の上、マシンサイクルを確認 @n
+				※基準クロックが32MHz以外の場合、@n
+				ループのデクリメントは８クロック必要なので、NOP を調整して @n
+				クロックを合わせる。 @n
+				※8MHzまでしか対応できない。
 	*/
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 	struct delay {
@@ -52,8 +61,9 @@ namespace utils {
 		*/
 		//-----------------------------------------------------------------//
 		static void milli_second(uint16_t ms) {
-			for(uint16_t i = 0; i < ms; ++i) {
+			while(ms > 0) {
 				micro_second(1000);
+				--ms;
 			}
 		}
 	};
