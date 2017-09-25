4fc2e057ac49d85e1875ee23785589eaa33ce8c3
hira <hira@rvf-rc45.net>
2017-09-26 04:17:42 +0900

    cleanup

--- WAV_PLAYER_sample/main.cpp
+++ WAV_PLAYER_sample/main.cpp
@@ -9,7 +9,6 @@
 				https://github.com/hirakuni45/RL78/blob/master/LICENSE
 */
 //=====================================================================//
-#include <cstdint>
 #include "common/renesas.hpp"
 #include "common/port_utils.hpp"
 #include "common/tau_io.hpp"
