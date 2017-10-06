0b6a26a55d445807ae1cb1cd87a13047494697c5
hira <hira@rvf-rc45.net>
2017-10-06 09:30:39 +0900

    update bit position manage

--- common/switch_man.hpp
+++ common/switch_man.hpp
@@ -15,24 +15,48 @@ namespace utils {
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 	/*!
 		@brief	スイッチ・マネージャー・クラス
-		@param[in]	BITS	 ビット定義クラス
+		@param[in]	BITS	ビット定義型（uint8_t, uint16_t, uint32_t)
+		@param[in]	NAME	ビット位置定義型（enum class）
 	*/
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
-	template <class BITS>
+	template <typename BITS, class NAME>
 	class switch_man {
 
 		BITS	level_;
 		BITS	positive_;
 		BITS	negative_;
 
+		static bool get_bit_(BITS v, NAME pos) noexcept
+		{
+			return (v & (1 << static_cast<BITS>(pos))) != 0; 
+		}
+
 	public:
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	コンストラクタ
+		*/
+		//-----------------------------------------------------------------//
+		switch_man() noexcept : level_(0), positive_(), negative_() { }
+
+
 		//-----------------------------------------------------------------//
 		/*!
 			@brief	レベルを取得
 			@return レベル
 		*/
 		//-----------------------------------------------------------------//
-		const BITS& get_level() const { return level_; }
+		const BITS& get_level() const noexcept { return level_; }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	レベルを取得
+			@param[in]	pos	ビット位置
+			@return 値
+		*/
+		//-----------------------------------------------------------------//
+		bool get_level(NAME pos) const noexcept { return get_bit_(level_, pos); }
 
 
 		//-----------------------------------------------------------------//
@@ -41,7 +65,17 @@ namespace utils {
 			@return 立ち上がり
 		*/
 		//-----------------------------------------------------------------//
-		const BITS& get_positive() const { return positive_; }
+		const BITS& get_positive() const noexcept { return positive_; }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	立ち上がりを取得（押した瞬間）
+			@param[in]	pos	ビット位置
+			@return 立ち上がり
+		*/
+		//-----------------------------------------------------------------//
+		bool get_positive(NAME pos) const noexcept { return get_bit_(positive_, pos); }
 
 
 		//-----------------------------------------------------------------//
@@ -50,7 +84,27 @@ namespace utils {
 			@return 立ち下がり
 		*/
 		//-----------------------------------------------------------------//
-		const BITS& get_negative() const { return negative_; }
+		const BITS& get_negative() const noexcept { return negative_; }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	立ち下がりを取得（離した瞬間）
+			@param[in]	pos	ビット位置
+			@return 立ち下がり
+		*/
+		//-----------------------------------------------------------------//
+		bool get_negative(NAME pos) const noexcept { return get_bit_(negative_, pos); }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	変化の取得
+			@param[in]	pos	ビット位置
+			@return 変化した場合「true」
+		*/
+		//-----------------------------------------------------------------//
+		bool get_turn(NAME pos) const noexcept { return get_bit_(positive_ | negative_, pos); }
 
 
 		//-----------------------------------------------------------------//
@@ -59,12 +113,11 @@ namespace utils {
 			@param[in]	level	スイッチ・レベル
 		*/
 		//-----------------------------------------------------------------//
-		void service(const BITS& level)
+		void service(BITS level) noexcept
 		{
-			positive_.preset( level() & ~level_());
-			negative_.preset(~level() &  level_());
+			positive_ = level & ~level_;
+			negative_ =~level &  level_;
 			level_ = level;
 		}
 	};
 }
-
