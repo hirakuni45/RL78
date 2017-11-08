236e117c7bff7547c713b438a8497ed7e4263018
hira <hira@rvf-rc45.net>
2017-11-09 08:53:44 +0900

    update input/output typedef

--- RECV_test/main.cpp
+++ RECV_test/main.cpp
@@ -14,8 +14,12 @@
 #include "common/uart_io.hpp"
 #include "common/tau_io.hpp"
 #include "common/iica_io.hpp"
+#include "common/csi_io.hpp"
 #include "common/format.hpp"
 #include "common/command.hpp"
+#include "common/switch_man.hpp"
+
+#include "chip/PCM1772.hpp"
 #include "chip/SEGMENT.hpp"
 
 namespace {
@@ -34,6 +38,7 @@ namespace {
 	UART1	uart1_;
 	UART2	uart2_;
 
+	// 出力ポート定義
 	typedef device::PORT<device::port_no::P7, device::bitpos::B3> SG1_A;
 	typedef device::PORT<device::port_no::P7, device::bitpos::B2> SG1_B;
 	typedef device::PORT<device::port_no::P7, device::bitpos::B1> SG1_C;
@@ -52,6 +57,9 @@ namespace {
 	typedef device::PORT<device::port_no::P7, device::bitpos::B4> SG2_G;
 	typedef chip::SEGMENT<SG2_A, SG2_B, SG2_C, SG2_D, SG2_E, SG2_F, SG2_G, device::NULL_PORT> SEG2;
 
+	typedef device::PORT<device::port_no::P2,device::bitpos::B1> MUTE;
+
+	// 入力ポート定義
 	typedef device::PORT<device::port_no::P1, device::bitpos::B0> LED_M1;
 	typedef device::PORT<device::port_no::P2, device::bitpos::B7> LED_M2;
 	typedef device::PORT<device::port_no::P2, device::bitpos::B4> LED_M3;
@@ -60,6 +68,64 @@ namespace {
 	typedef device::PORT<device::port_no::P4, device::bitpos::B6> CH_SEL;
 	typedef device::PORT<device::port_no::P4, device::bitpos::B5> IR_TEST;
 	typedef device::PORT<device::port_no::P4, device::bitpos::B4> CH_SCAN;
+	typedef device::PORT<device::port_no::P4, device::bitpos::B3> L_M;
+
+	typedef device::PORT<device::port_no::P12, device::bitpos::B4> S_M1;
+	typedef device::PORT<device::port_no::P12, device::bitpos::B3> S_M2;
+	typedef device::PORT<device::port_no::P13, device::bitpos::B7> S_M3;
+	typedef device::PORT<device::port_no::P12, device::bitpos::B2> S_M4;
+
+	typedef device::PORT<device::port_no::P12, device::bitpos::B1> MSEL;
+
+	enum class INPUT_TYPE : uint8_t {
+		CH_SEL,
+		IR_TEST,
+		CH_SCAN,
+
+		L_M,	///< MIC/LINE
+
+		S_M1,
+		S_M2,
+		S_M3,
+		S_M4,
+
+		MSEL,	///< モジュール選択、L:1個、H:2個
+	};
+
+	typedef utils::switch_man<uint16_t, INPUT_TYPE> INPUT;
+	INPUT	input_;
+
+	void service_input_()
+	{
+		uint16_t lvl = 0;
+
+		if(CH_SEL::P() ) lvl |= 1 << static_cast<uint8_t>(INPUT_TYPE::CH_SEL);
+		if(IR_TEST::P()) lvl |= 1 << static_cast<uint8_t>(INPUT_TYPE::IR_TEST);
+		if(CH_SCAN::P()) lvl |= 1 << static_cast<uint8_t>(INPUT_TYPE::CH_SCAN);
+
+		if(L_M::P()) lvl |= 1 << static_cast<uint8_t>(INPUT_TYPE::L_M);
+
+		if(S_M1::P() ) lvl |= 1 << static_cast<uint8_t>(INPUT_TYPE::S_M1);
+		if(S_M2::P() ) lvl |= 1 << static_cast<uint8_t>(INPUT_TYPE::S_M2);
+		if(S_M3::P() ) lvl |= 1 << static_cast<uint8_t>(INPUT_TYPE::S_M3);
+		if(S_M4::P() ) lvl |= 1 << static_cast<uint8_t>(INPUT_TYPE::S_M4);
+
+		if(MSEL::P()) lvl |= 1 << static_cast<uint8_t>(INPUT_TYPE::MSEL);
+
+		input_.service(lvl);
+	}
+
+	// CSI(SPI) の定義、CSI30 の通信では、「SAU12」を利用、１ユニット、チャネル２
+	typedef device::csi_io<device::SAU12> CSI;
+	CSI		csi_;
+
+	// PCM1772 デバイス選択（P34 ---> PCM1772:/MS(15)）
+	typedef device::PORT<device::port_no::P3, device::bitpos::B4> PCM_SEL;
+	// PCM1772 パワー・ダウン（P30 ---> PCM1772:/PD(?))
+	typedef device::PORT<device::port_no::P3, device::bitpos::B0> PCM_PD;
+
+	typedef chip::PCM1772<CSI, PCM_SEL, PCM_PD> PCM;
+	PCM		pcm_(csi_);
 
 	utils::command<64> command_;
 }
@@ -195,7 +261,22 @@ int main(int argc, char* argv[])
 	LED_M3::DIR = 1;
 	LED_M4::DIR = 1;
 
+	MUTE::DIR = 1;
+	MUTE::P = 1;
+
 	utils::format("\nStart Digital MIC Reciver Version: %d.%02d\n") % (VERSION / 100) % (VERSION % 100);
+
+	// CSI 開始
+	{
+		uint8_t intr_level = 0;
+		if(!csi_.start(4000000, CSI::PHASE::TYPE4, intr_level)) {
+			utils::format("CSI Start fail...\n");
+		}
+	}
+
+	// PCM1772 初期化
+	pcm_.start();
+
 	command_.set_prompt("# ");
 
 	uint8_t	n = 0;
@@ -203,15 +284,37 @@ int main(int argc, char* argv[])
 	while(1) {
 		itm_.sync();
 
-		if(CH_SEL::P()) {
+		service_input_();
+
+		if(input_.get_positive(INPUT_TYPE::CH_SEL)) {
 			utils::format("CH_SEL\n");
 		}
-		if(IR_TEST::P()) {
+		if(input_.get_positive(INPUT_TYPE::IR_TEST)) {
 			utils::format("IR_TEST\n");
 		}
-		if(CH_SCAN::P()) {
+		if(input_.get_positive(INPUT_TYPE::CH_SCAN)) {
 			utils::format("CH_SCAN\n");
 		}
+
+		if(input_.get_turn(INPUT_TYPE::L_M)) {
+			utils::format("L_M: %d\n") % input_.get_level(INPUT_TYPE::L_M);
+		}
+		if(input_.get_turn(INPUT_TYPE::S_M1)) {
+			utils::format("S_M1: %d\n") % input_.get_level(INPUT_TYPE::S_M1);
+		}
+		if(input_.get_turn(INPUT_TYPE::S_M2)) {
+			utils::format("S_M2: %d\n") % input_.get_level(INPUT_TYPE::S_M2);
+		}
+		if(input_.get_turn(INPUT_TYPE::S_M3)) {
+			utils::format("S_M3: %d\n") % input_.get_level(INPUT_TYPE::S_M3);
+		}
+		if(input_.get_turn(INPUT_TYPE::S_M4)) {
+			utils::format("S_M4: %d\n") % input_.get_level(INPUT_TYPE::S_M4);
+		}
+		if(input_.get_turn(INPUT_TYPE::MSEL)) {
+			utils::format("MSEL: %d\n") % input_.get_level(INPUT_TYPE::MSEL);
+		}
+
 
 
 		SEG1::decimal(t / 10);
