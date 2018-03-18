be30e9ce8f89e1c3f7ecfa40cd71c6a32472290d
hira <hira@rvf-rc45.net>
2018-03-18 23:10:03 +0900

    update: cleanup

--- common/flash_io.hpp
+++ common/flash_io.hpp
@@ -1,7 +1,7 @@
 #pragma once
 //=====================================================================//
 /*!	@file
-	@brief	RL78/G13 グループ・データ・フラッシュ制御
+	@brief	RL78 グループ・データ・フラッシュ制御
     @author 平松邦仁 (hira@rvf-rc45.net)
 	@copyright	Copyright (C) 2017 Kunihito Hiramatsu @n
 				Released under the MIT license @n
